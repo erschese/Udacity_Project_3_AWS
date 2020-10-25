c40813e135f0fb15c64e769fd36f5e413b77e078
Home <serschen@yahoo.de>
2020-10-25 09:47:49 +0100

    feat: -------------------------- *file structure reorganisation
    description:
    --------------------------
    --
    
    issue tracker:
    --------------------------
        Resolves: -
        See also: -

--- 011_create_redshift_cluster.py
+++ 011_create_redshift_cluster.py
@@ -1,7 +1,7 @@
-# This is a sample Python script.
+# This is a sample Python script
 import myAWSKit
 
-# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script
 if __name__ == '__main__':
 
     # get parameters
